38552ef51325289a7fd9cc7f97a519852ad5aefa
josevargas1229 <20221015@uthh.edu.mx>
2025-06-07 00:10:57 -0600

    Se añade consulta no. autorizacion

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const dbService = require('../services/dbService');
 
+
+router.get('/concesion/autorizacion/:id', async (req, res) => {
+    try {
+        const result = await dbService.obtenerConcesionPorId(req.params.id);
+        if (!result.data) {
+            return res.status(404).json({ message: 'Concesión no encontrada', returnValue: result.returnValue });
+        }
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al obtener la concesión' });
+    }
+});
+
 router.get('/concesion/:id', async (req, res) => {
     try {
         const result = await dbService.obtenerInformacionCompletaPorConcesion(req.params.id);
